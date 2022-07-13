8802f2a98e865b4b620072bd14300924a9757f5d
Rimantas <belauzas@yahoo.com>
2022-07-13 11:18:12 +0300

    Initial commit

---
+++ js/Kaitlente.js
@@ -0,0 +1,13 @@
+class Kaitlente {
+    constructor(skyliuKiekis) {
+        this.size = skyliuKiekis;
+
+        console.log(this);
+    }
+
+    render() {
+        console.log('piesiam turini...');
+    }
+}
+
+export { Kaitlente }
---
+++ js/main.js
@@ -0,0 +1,14 @@
+import { Kaitlente } from "./Kaitlente.js";
+
+const kaitlentciuKiekis = 5;
+
+// h1 -> Virtuve
+// h1 -> Dirbtuve (5)
+
+const titleDOM = document.querySelector('.title');
+console.log(titleDOM);
+
+// for (let i = 1; i <= kaitlentciuKiekis; i++) {
+//     const irenginys = new Kaitlente(i);
+//     irenginys.render();
+// }
